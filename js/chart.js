6d7cfaa9a8918e1610f4c4d8a8541de6f21a6670
chwasiq0569 <samsunggrandprimeplus056@gmail.com>
2021-02-23 01:07:58 +0500

    Created Privacy Page

--- js/chart.js
+++ js/chart.js
@@ -45,23 +45,24 @@ jQuery( document ).ready( function() {
                 labels: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'],
                 datasets: [{
                     label: 'My First dataset',
-                    // showLine: false,	
                     fill: true,
-                    backgroundColor: 'rgba(34, 37, 33, 0.1)',
+                    // backgroundColor: 'rgb(163, 166, 168, 0.35)',
+                    backgroundColor: '#DFE0E1',
                     pointRadius: 0,
                     borderWidth: 4,
-                    borderColor: 'rgba(34, 37, 33, 0.6)',
+                    borderColor: 'rgb(163, 166, 168, 0.75)',
                     data: [5 ,5, 5 , 16, 17,19, 35, 45, 38, 65, 62, 58, 75]
                 },
                 {
                     label: 'My Second dataset',
                 //    data: [28, 48, 40, 19, 86, 27, 90],
                 // backgroundColor: 'rgba(0,148,106, 0.65)',
-                    backgroundColor: "rgba(113, 166, 152,0.55)",
+                    backgroundColor: "rgb(1, 165, 124, 0.45)",
                     fill: true,
                     pointRadius: 0,
-                    // borderColor: '#00A97F',
-                    borderColor: 'rgb(113, 166, 152)',
+                    // backgroundColor: "white",
+                    borderColor: '#00A97F',
+                    // borderColor: 'rgb(1, 165, 124)',
                     borderWidth: 4,
                     data: [0 ,1, 3, 11, 10, 15, 28, 38, 30, 48, 50, 50, 68]
                 }]
